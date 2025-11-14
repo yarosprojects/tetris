d557a49b61736d35ba05c5876d23e9115bf8ecb8
Yaroslav Klymko <yaros.trabajo22@gmail.com>
2025-11-14 20:30:56 +0100

    Modifying config

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter({ fallback: 'index.html' }),
-  }
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter()
+	}
 };
+
+export default config;
